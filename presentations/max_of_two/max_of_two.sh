cb95e8d047c321c0e951d166d6934a39c91f13c5
ledmer <ledmer13@gmail.com>
2024-11-06 12:11:16 -0400

    notes

---
+++ presentations/max_of_two/max_of_two.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Define the file path to be used as input
+FILE_PATH="/mnt/c/Users/deerl/Downloads/"
+
+# Run the Python script with the file path as an argument
+python /home/ledmer/High_School/2024-2025/CSC215/presentations/max_of_two/max_of_two.py "$FILE_PATH"
